7c952a0b4f6e8534afca9138dcd25df169614d86
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-21 11:29:35 +0000

    End of recap

--- src/app/food/page.tsx
+++ src/app/food/page.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
+type cakesSearchQuery = {
+	sortBy: string;
+};
+
 type cakesType = {
 	name: string;
 	id: number;
@@ -20,11 +24,42 @@ let cakes: cakesType[] = [
 	},
 ];
 
-export default function FoodRoute() {
+function compareCakes(a: cakesType, b: cakesType) {
+	if (a.name < b.name) {
+		return -1;
+	} else if (a.name > b.name) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+export default function Page({
+	searchParams,
+}: {
+	searchParams: cakesSearchQuery;
+}) {
+	let sortedCakes = [...cakes];
+
+	if (searchParams.sortBy == "asc") {
+		sortedCakes.sort(compareCakes);
+	} else if (searchParams.sortBy == "desc") {
+		sortedCakes.sort(compareCakes).reverse();
+	}
+
 	return (
 		<div>
 			<h1>Food is nice</h1>
-			{cakes.map((cake) => {
+			{/* <p>You have selected {searchParams.sortBy}</p> */}
+			<br />
+			<Link href="/food">Reset</Link>
+			<br />
+			<Link href="/food?sortBy=asc">Sort by ascending</Link>
+			<br />
+			<Link href="/food?sortBy=desc">Sort by descending</Link>
+			<br />
+
+			{sortedCakes.map((cake) => {
 				return (
 					<div key={cake.id}>
 						<br />
